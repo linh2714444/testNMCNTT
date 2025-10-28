f8df5b3f7adbfea2ee1e3e2790d7bd6509bde939
linh2714444 <linh2714444@gmail.com>
2025-10-28 15:39:51 +0700

    first commit

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    int n, k;
+    cin >> n >> k;
+    int a[500000];
+    for (int i = 1; i <= n; i++)
+        cin >> a[i];
+    sort(a + 1, a + n + 1, greater<int>());
+    if (n < k)
+        cout << "Not found";
+    else
+        cout << a[k];
+    return 0;
+}
